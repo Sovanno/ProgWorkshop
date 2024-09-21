43075e1f1ac9d0bbe7f31de3d4f7e1384ed28c9d
Fink Eduard <fink.2004@mail.ru>
2024-09-21 10:26:20 +0600

    add Login Page and tests for Login

---
+++ main.py
@@ -0,0 +1,41 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+'''
+import re
+from playwright.sync_api import Playwright, sync_playwright, expect
+
+
+def run(playwright: Playwright) -> None:
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context()
+    page = context.new_page()
+    page.goto("https://www.saucedemo.com/")
+    page.locator("[data-test=\"username\"]").click()
+    page.locator("[data-test=\"username\"]").fill("standard_user")
+    page.locator("[data-test=\"password\"]").click()
+    page.locator("[data-test=\"password\"]").fill("secret_sauce")
+    page.locator("[data-test=\"password\"]").press("Enter")
+
+    # ---------------------
+    context.close()
+    browser.close()
+
+
+with sync_playwright() as playwright:
+    run(playwright)
+
+'''
---
+++ tests/test_login_error.py
@@ -0,0 +1,9 @@
+from playwright.sync_api import Page, expect
+from pages.LoginPage import LoginPage
+
+
+def test_error_login(page: Page) -> None:
+    login_page = LoginPage(page)
+    login_page.navigate()
+    login_page.login("ASDQWERTZXCV", "asdwqfavzxv")
+    expect(login_page.error).to_be_visible()
