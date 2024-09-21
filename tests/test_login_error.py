e6c09e73865bde2c7ea18e2f00b4dddc18b6a0a5
Fink Eduard <fink.2004@mail.ru>
2024-09-21 11:02:54 +0600

    fix Login tests and add report

--- tests/test_login_error.py
+++ tests/test_login_error.py
@@ -1,7 +1,6 @@
 from playwright.sync_api import Page, expect
 from pages.LoginPage import LoginPage
 
-
 def test_error_login(page: Page) -> None:
     login_page = LoginPage(page)
     login_page.navigate()
